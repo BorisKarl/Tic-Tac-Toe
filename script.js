e2b5e6a75a99706ac8aefb27c249928a87a23332
BorisKarl <herrnielsen@web.de>
2023-04-24 09:54:20 +0200

    einrichten der eventListener, Testfunktion, Wechsel i auf X, noch keine Player functions

---
+++ script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const game_board = document.getElementById('game_board');
+const msg = document.getElementById('msg');
+
+const showData = (e) => {
+    let data = e.target.id;
+    console.log(data)
+    return data;
+}
+
+
+const playfield = (() =>  {
+    const board = ["", "", "", "", "", "", "", "", ""];
+})();
+
+const Player = (sign) => {
+
+
+}
+
+const x_sign = (() => {
+    for (let i = 1; i < 10; i++) {
+        let eDiv = document.getElementById(i);
+        eDiv.addEventListener('click', (e) => {
+            if (e.target.innerHTML == i) {
+                e.target.innerHTML = "X";
+                msg.innerHTML = "Player O turn";
+            }
+            else {
+                e.target.innerHTML = i;
+                msg.innerHTML = "Player X turn";
+            }
+            
+        })
+    }
+})();
+
+const number_sign = (() => {
+    for (let i = 1; i < 10; i++) {
+        let eDiv = document.getElementById(i);
+        eDiv.addEventListener('click', (e) => {
+            if (e.target.innerHTML > "0" || e.target.innerHTML < "10")
+            {
+                return;
+            }
+            else
+            {
+                e.target.innerHTML = i;
+            }
+            
+        })
+    }
+})();
+
+
+
+/* 
+// This works inside the playfield function
++ const player_move = () => {
++     player_square = document.getElementById(showData);
++     player_square.innerHTML = "X";
+}; 
+one.addEventListener('click', player_move);
+
+*/
+ 
+
+const fill_array = () => {
+    let square_array = [];
+    for(let i = 0; i < 9; i++) {
+
+    }
+}
+
+ 
+const one = document.getElementById('1');
+const brett = document.createElement('div');
+
+
+
+
+/*
+one.addEventListener('click', (e) => {
+    document.getElementById(e.target.id).innerHTML = "X";})
+ +const one = document.getElementById('1')
+
+const one = document.getElementById('1')
+const one = document.getElementById('1')
+const one = document.getElementById('1')
+const one = document.getElementById('1')
+square.addEventListener('click', player_move);
+ */
+
+
+
+
+
+
+
+
+
